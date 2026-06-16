f39b792770427da2a1d790f6fb3610184a65f908
Somesh Jadhav <someshj700@gmail.com>
2026-06-16 07:44:14 +0530

    current

--- src/test/java/Selenium/Get_Current_Page_Title.java
+++ src/test/java/Selenium/Get_Current_Page_Title.java
@@ -3,20 +3,28 @@ package Selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class Get_Current_Page_Title {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver();
 
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
 
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.amazon.in/gp/bestsellers/?ref_=nav_em_cs_bestsellers_0_1_1_2");
 
         String title=driver.getTitle();
         System.out.println("Get_Current_Page_Title:" +title);
         driver.navigate().refresh();
 
+        Thread.sleep(5000);
         driver.quit();
     }
 }
