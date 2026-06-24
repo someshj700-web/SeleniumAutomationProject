d10a8cec24ba964a91d41dc7c39f4b461af6b5e4
Somesh Jadhav <someshj700@gmail.com>
2026-06-24 18:21:36 +0530

    soma

---
+++ src/test/java/TestNg/priority.java
@@ -0,0 +1,35 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class priority {
+
+    @Test
+    public void zebra(){
+        System.out.println("zebra");
+    }
+    @Test
+    public void yummy(){
+        System.out.println("yummy");
+    }
+
+    @Test
+    public void xerox(){
+        System.out.println("xerox");
+    }
+
+    @Test
+    public void woodland(){
+        System.out.println("woodland");
+    }
+
+    @Test
+    public void venom(){
+        System.out.println("venom");
+    }
+
+    @Test
+    public void unicorn(){
+        System.out.println("zebra");
+    }
+}
